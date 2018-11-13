099998194dff346287fb617c8a9fa086760e5e4a
Lam Thieu <lamthieu@kms.com.vn>
2018-11-13 14:21:06 +0700

    use data model with sqlite3

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class LogMessage(models.Model):
+    message = models.CharField(max_length=300)
+    log_date = models.DateTimeField("date logged")
+
+    def __str__(self):
+        """Returns a string representation of a message."""
+        return f"'{self.message}' logged on {self.log_date.strftime('%A, %d %B, %Y at %X')}"
+        
--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 import re
 from datetime import datetime
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 def about(request):
     return render(request, "hello/about.html")
@@ -21,3 +30,16 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+def log_message(request):
+    if request.method == "POST":
+        form = LogMessageForm(request.POST)
+
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        form = LogMessageForm()
+        return render(request, "hello/log_message.html", {"form": form})
