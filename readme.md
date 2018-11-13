48d5c4d08c091c8c799aae24f642e633356069ac
Lam Thieu <lamthieu@kms.com.vn>
2018-11-13 14:36:58 +0700

    add admin default site

---
+++ readme.md
@@ -0,0 +1,7 @@
+http://127.0.0.1:8000/admin/
+
+superuser account:
+user: qlam
+email: quanglam.thieu
+password: 123123123
+
--- web_project/urls.py
+++ web_project/urls.py
@@ -19,5 +19,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("",include("hello.urls")),
+    path("admin/", admin.site.urls),
 ]
 urlpatterns += staticfiles_urlpatterns()
