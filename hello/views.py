4e606d485e5d6c198ab11001732c326307dd0849
Lam Thieu <lamthieu@kms.com.vn>
2018-11-13 11:12:16 +0700

    finish simple web django

---
+++ hello/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import re
+from datetime import datetime
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
